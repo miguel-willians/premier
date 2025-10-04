34e37e17d7e989735aa922e2767d81f8836f0380
miguel-willians <miiguel.wso@gmail.com>
2025-10-04 09:39:47 -0300

    feat: add all pages in navigator

--- App.js
+++ App.js
@@ -8,27 +8,63 @@ import Home from './src/pages/Home/index';
 import Produtos from './src/pages/Produtos/index';
 import CadastrarProduto from './src/pages/CadastrarProduto/index';
 import Entrada from './src/pages/Entrada/index';
+import Relatorios from './src/pages/Relatorios/index';
+import Saida from './src/pages/Saida/index';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-    
-      <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Saida">
       <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            header: () => <Header>Premier</Header>,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+      <Stack.Screen
+        name="Produtos"
+        component={Produtos}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+      <Stack.Screen
+        name="CadastrarProduto"
+        component={CadastrarProduto}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+      <Stack.Screen
+        name="Entrada"
+        component={Entrada}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+      <Stack.Screen
+        name="Relatorios"
+        component={Relatorios}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+      <Stack.Screen
+        name="Saida"
+        component={Saida}
+        options={{
+          header: () => <Header>Premier</Header>,
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
